a376e25eadcc11dec3c361e32e1faadcf2f8f7ae
tms-tienpham <tien.pham@tomosia.com>
2025-07-02 11:38:51 +0700

    Featute module for project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $modulesPath = base_path('Modules');
+        if (is_dir($modulesPath)) {
+            foreach (scandir($modulesPath) as $module) {
+                if ($module === '.' || $module === '..') continue;
+                $provider = "Modules\\{$module}\\Providers\\{$module}ServiceProvider";
+                $providerPath = $modulesPath . "/{$module}/Providers/{$module}ServiceProvider.php";
+                if (file_exists($providerPath)) {
+                    $this->app->register($provider);
+                }
+            }
+        }
     }
 
     /**
