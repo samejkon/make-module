8421633f67bc0c0b70005956c00157407a10c52a
phai-le <phai.le@tomosia.com>
2025-08-23 11:07:51 +0700

    Feature: database

---
+++ database/migrations/2025_08_23_034825_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('store_id');
+            $table->integer('customer_id')->nullable();
+            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->enum('payment_status', ['unpaid','paid','partial'])->default('unpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
